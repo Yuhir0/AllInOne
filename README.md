c7b21bd708cceaccb021f957ba168085169aecc5
Óscar García <oscar.garcia@basetis.com>
2025-03-10 09:51:06 +0100

    doc: Added readmes

---
+++ README.md
@@ -0,0 +1,16 @@
+# All in One
+
+## Overview
+
+This project provides a collection of utilities i used and found interest to
+keep and share, these utilities includes:
+
+- Viewing HTML content from the clipboard in a browser.
+- Extracting SQL table names, aliases, and columns from a query.
+- Safe access to object attributes and methods to avoid `NoneType` errors.
+- Custom file locking mechanism for safe access to resources.
+
+
+## To be used
+
+A specific readme are placed in each directory to explain how to use the utility
--- hooks/check_alembic_revisions.py
+++ hooks/check_alembic_revisions.py
@@ -2,25 +2,27 @@ import re
 import subprocess
 import sys
 
-# Change the regex to match the revision format of your project
-revision_regex = re.compile(r"\d{14}")
+revision_regex = re.compile(r"([A-z0-9_-]+)(\s\(head\))")
 
 
 def main():
-    heads = revision_regex.findall(_run_alembic_heads())
+    heads = _get_heads()
     print("HEADS:", *heads, sep="\n")
 
     if len(heads) > 1:
         print("There are more than one head revision. Fix it merging it or setting in order.")
         sys.exit(1)
 
-    current = revision_regex.search(_run_alembic_current()).group(0)
+    current = _get_current()
 
     if current not in heads:
         print("Current revision is not in heads. Run 'alembic upgrade head' to fix it.")
         sys.exit(1)
-
     print("Revisions are in order.")
+
+
+def _get_heads() -> list[str]:
+    return [head.group(1) for head in revision_regex.finditer(_run_alembic_heads())]
 
 
 def _run_alembic_heads() -> str:
@@ -29,6 +31,12 @@ def _run_alembic_heads() -> str:
         print("Error:", result.stderr)
         sys.exit(result.returncode)
     return result.stdout
+
+
+def _get_current() -> str | None:
+    if res := revision_regex.search(_run_alembic_current()):
+        return res.group(1)
+    return None
 
 
 def _run_alembic_current() -> str:
